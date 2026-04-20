8dd421dfc45481042cd3ed4cf29a02f0b479af81
LaxmiPrasanna Bandam <24wh1a05w6@bvrithyderabad.edu.in>
2026-04-20 19:48:25 +0530

    add api chat route

--- api/chat.js
+++ api/chat.js
@@ -1,15 +1,11 @@
 export default function handler(req, res) {
-  try {
-    if (req.method !== "POST") {
-      return res.status(405).json({ error: "Only POST allowed" });
-    }
-
+  if (req.method === "POST") {
     const { message } = req.body;
 
     return res.status(200).json({
-      reply: "Server working 🚀 You said: " + message,
+      reply: "Working 🚀 You said: " + message,
     });
-  } catch (err) {
-    return res.status(500).json({ error: "Server crashed" });
   }
+
+  res.status(405).json({ error: "Only POST allowed" });
 }
